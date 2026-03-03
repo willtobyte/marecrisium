0fb837d10f6e624d51f719139c801799354bacac
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-03 13:44:14 -0300

    Work in progress

--- cartridge/objects/namespace/player.lua
+++ cartridge/objects/namespace/player.lua
@@ -1,43 +1,45 @@
 return {
 	animation = {
 		running = {
-			{ x, y, w, h, duration, cx, cy, cw, ch }, -- x, y, w, h on spritesheet. duration is duration of frame in ms. cx, cy, cw, ch are collision quad
+			-- x, y, w, h on the spritesheet; duration is the frame duration in ms; cx, cy, cw, ch define the collision quad
+			{ x, y, w, h, duration, cx, cy, cw, ch },
 		},
 	},
 
 	on_spawn = function(self)
+		-- Called once when the object is spawned
 		self.animation = "running"
 	end,
 
 	on_loop = function(self, delta)
-		-- every frame
+		-- Called every frame with the elapsed time since the last frame
 	end,
 
 	on_animation_begin = function(self, animation)
-		-- when the animation begins, animation is its name
+		-- Called when an animation begins; animation is its name
 	end,
 
 	on_animation_end = function(self, animation)
-		-- when the animation ends, animation is its name
+		-- Called when an animation ends; animation is its name
 	end,
 
 	on_collision_begin = function(self, name, kind)
-		-- when collision begins, name is the other object's name and kind is the other object's kind
+		-- Called when a collision begins; name is the other object's name and kind is the other object's kind
 	end,
 
 	on_collision_end = function(self, name, kind)
-		-- when collision ends, name is the other object's name and kind is the other object's kind
+		-- Called when a collision ends; name is the other object's name and kind is the other object's kind
 	end,
 
 	on_screen_exit = function(self, direction)
-		-- when the object exits the screen, direction is the exit direction
+		-- Called when the object exits the screen; direction is the exit direction
 	end,
 
 	on_screen_enter = function(self, direction)
-		-- when the object enters the screen, direction is the enter direction
+		-- Called when the object enters the screen; direction is the entry direction
 	end,
 
 	on_damage = function(self, amount)
-		-- when pool.player:damage(10) is called
+		-- Called when the object takes damage, e.g. pool.player:damage(10)
 	end,
 }
---
+++ cartridge/stages/namespace.lua
@@ -0,0 +1,13 @@
+return {
+	on_enter = function()
+		-- Called once when the stage is entered
+	end,
+
+	on_loop = function(self, delta)
+		-- Called every frame with the elapsed time since the last frame
+	end,
+
+	on_leave = function()
+		-- Called once when the stage is left
+	end,
+}
