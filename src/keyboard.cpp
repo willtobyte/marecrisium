27f23cf3f0fef5ba172986c048c6f4626736b3c7
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-04 13:57:30 -0300

    Work in progress

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,36 +1,61 @@
 #include "keyboard.hpp"
 
-static const std::unordered_map<std::string_view, SDL_Scancode> mapping{
-  {"a", SDL_SCANCODE_A}, {"b", SDL_SCANCODE_B}, {"c", SDL_SCANCODE_C}, {"d", SDL_SCANCODE_D},
-  {"e", SDL_SCANCODE_E}, {"f", SDL_SCANCODE_F}, {"g", SDL_SCANCODE_G}, {"h", SDL_SCANCODE_H},
-  {"i", SDL_SCANCODE_I}, {"j", SDL_SCANCODE_J}, {"k", SDL_SCANCODE_K}, {"l", SDL_SCANCODE_L},
-  {"m", SDL_SCANCODE_M}, {"n", SDL_SCANCODE_N}, {"o", SDL_SCANCODE_O}, {"p", SDL_SCANCODE_P},
-  {"q", SDL_SCANCODE_Q}, {"r", SDL_SCANCODE_R}, {"s", SDL_SCANCODE_S}, {"t", SDL_SCANCODE_T},
-  {"u", SDL_SCANCODE_U}, {"v", SDL_SCANCODE_V}, {"w", SDL_SCANCODE_W}, {"x", SDL_SCANCODE_X},
-  {"y", SDL_SCANCODE_Y}, {"z", SDL_SCANCODE_Z},
-
-  {"0", SDL_SCANCODE_0}, {"1", SDL_SCANCODE_1}, {"2", SDL_SCANCODE_2}, {"3", SDL_SCANCODE_3},
-  {"4", SDL_SCANCODE_4}, {"5", SDL_SCANCODE_5}, {"6", SDL_SCANCODE_6}, {"7", SDL_SCANCODE_7},
-  {"8", SDL_SCANCODE_8}, {"9", SDL_SCANCODE_9},
-
-  {"up", SDL_SCANCODE_UP}, {"down", SDL_SCANCODE_DOWN},
-  {"left", SDL_SCANCODE_LEFT}, {"right", SDL_SCANCODE_RIGHT},
-
-  {"shift", SDL_SCANCODE_LSHIFT}, {"ctrl", SDL_SCANCODE_LCTRL},
-
-  {"escape", SDL_SCANCODE_ESCAPE}, {"space", SDL_SCANCODE_SPACE},
-  {"enter", SDL_SCANCODE_RETURN}, {"backspace", SDL_SCANCODE_BACKSPACE},
-  {"tab", SDL_SCANCODE_TAB}
-};
-
 static int keyboard_index(lua_State *state) {
-  const auto *key = luaL_checkstring(state, 2);
-  const auto it = mapping.find(key);
-  if (it == mapping.end()) [[unlikely]]
+  const std::string_view key = luaL_checkstring(state, 2);
+  SDL_Scancode code;
+
+  if (key == "a") code = SDL_SCANCODE_A;
+  else if (key == "b") code = SDL_SCANCODE_B;
+  else if (key == "c") code = SDL_SCANCODE_C;
+  else if (key == "d") code = SDL_SCANCODE_D;
+  else if (key == "e") code = SDL_SCANCODE_E;
+  else if (key == "f") code = SDL_SCANCODE_F;
+  else if (key == "g") code = SDL_SCANCODE_G;
+  else if (key == "h") code = SDL_SCANCODE_H;
+  else if (key == "i") code = SDL_SCANCODE_I;
+  else if (key == "j") code = SDL_SCANCODE_J;
+  else if (key == "k") code = SDL_SCANCODE_K;
+  else if (key == "l") code = SDL_SCANCODE_L;
+  else if (key == "m") code = SDL_SCANCODE_M;
+  else if (key == "n") code = SDL_SCANCODE_N;
+  else if (key == "o") code = SDL_SCANCODE_O;
+  else if (key == "p") code = SDL_SCANCODE_P;
+  else if (key == "q") code = SDL_SCANCODE_Q;
+  else if (key == "r") code = SDL_SCANCODE_R;
+  else if (key == "s") code = SDL_SCANCODE_S;
+  else if (key == "t") code = SDL_SCANCODE_T;
+  else if (key == "u") code = SDL_SCANCODE_U;
+  else if (key == "v") code = SDL_SCANCODE_V;
+  else if (key == "w") code = SDL_SCANCODE_W;
+  else if (key == "x") code = SDL_SCANCODE_X;
+  else if (key == "y") code = SDL_SCANCODE_Y;
+  else if (key == "z") code = SDL_SCANCODE_Z;
+  else if (key == "0") code = SDL_SCANCODE_0;
+  else if (key == "1") code = SDL_SCANCODE_1;
+  else if (key == "2") code = SDL_SCANCODE_2;
+  else if (key == "3") code = SDL_SCANCODE_3;
+  else if (key == "4") code = SDL_SCANCODE_4;
+  else if (key == "5") code = SDL_SCANCODE_5;
+  else if (key == "6") code = SDL_SCANCODE_6;
+  else if (key == "7") code = SDL_SCANCODE_7;
+  else if (key == "8") code = SDL_SCANCODE_8;
+  else if (key == "9") code = SDL_SCANCODE_9;
+  else if (key == "up") code = SDL_SCANCODE_UP;
+  else if (key == "down") code = SDL_SCANCODE_DOWN;
+  else if (key == "left") code = SDL_SCANCODE_LEFT;
+  else if (key == "right") code = SDL_SCANCODE_RIGHT;
+  else if (key == "shift") code = SDL_SCANCODE_LSHIFT;
+  else if (key == "ctrl") code = SDL_SCANCODE_LCTRL;
+  else if (key == "escape") code = SDL_SCANCODE_ESCAPE;
+  else if (key == "space") code = SDL_SCANCODE_SPACE;
+  else if (key == "enter") code = SDL_SCANCODE_RETURN;
+  else if (key == "backspace") code = SDL_SCANCODE_BACKSPACE;
+  else if (key == "tab") code = SDL_SCANCODE_TAB;
+  else [[unlikely]]
     return lua_pushnil(state), 1;
 
   const auto *keyboard = SDL_GetKeyboardState(nullptr);
-  lua_pushboolean(state, keyboard[it->second]);
+  lua_pushboolean(state, keyboard[code]);
   return 1;
 }
 
