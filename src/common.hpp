bd5863e886e17082a9d8f5db35f3364010fd4904
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-05 17:26:43 -0300

    Work in progress

--- src/common.hpp
+++ src/common.hpp
@@ -41,6 +41,38 @@ struct viewport {
 
 extern struct viewport viewport;
 
+struct SDL_Deleter final {
+  template <typename T>
+  void operator()(T* ptr) const noexcept {
+    if (!ptr) [[unlikely]] return;
+
+    if constexpr (requires { SDL_CloseGamepad(ptr); }) SDL_CloseGamepad(ptr);
+    else if constexpr (requires { SDL_DestroyTexture(ptr); }) SDL_DestroyTexture(ptr);
+    else if constexpr (requires { SDL_free(ptr); }) SDL_free(ptr);
+  }
+};
+
+struct SPNG_Deleter final {
+  void operator()(spng_ctx* ctx) const noexcept {
+    if (!ctx) [[unlikely]] return;
+
+    spng_ctx_free(ctx);
+  }
+};
+
+struct PHYSFS_Deleter final {
+  template <typename T>
+  void operator()(T* ptr) const noexcept {
+    if (!ptr) [[unlikely]] return;
+
+    if constexpr (std::is_same_v<T, PHYSFS_File>) {
+      PHYSFS_close(ptr);
+    } else if constexpr (std::is_same_v<T, char*>) {
+      PHYSFS_freeList(ptr);
+    }
+  }
+};
+
 struct transparent_hash final {
   using is_transparent = void;
   auto operator()(std::string_view sv) const noexcept { return std::hash<std::string_view>{}(sv); }
