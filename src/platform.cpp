e8a703f3017547f72c46a68ed0ee15be61cbe9af
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-05 13:02:15 -0300

    Work in progress

---
+++ src/platform.cpp
@@ -0,0 +1,61 @@
+#include "platform.hpp"
+
+static int platform_index(lua_State *state) noexcept {
+  const std::string_view key = luaL_checkstring(state, 2);
+
+  if (key == "name")
+    return lua_pushstring(state, SDL_GetPlatform()), 1;
+
+  if (key == "cores")
+    return lua_pushinteger(state, SDL_GetNumLogicalCPUCores()), 1;
+
+  if (key == "memory")
+    return lua_pushinteger(state, SDL_GetSystemRAM()), 1;
+
+  if (key == "locale") {
+    auto count = 0;
+    const auto locales = std::unique_ptr<SDL_Locale*[], SDL_Deleter>(SDL_GetPreferredLocales(&count));
+    if (!locales || count == 0 || !locales[0]->language) [[unlikely]]
+      return lua_pushstring(state, ""), 1;
+
+    if (locales[0]->country) {
+      const auto result = std::format("{}-{}", locales[0]->language, locales[0]->country);
+      lua_pushstring(state, result.c_str());
+    } else {
+      lua_pushstring(state, locales[0]->language);
+    }
+    return 1;
+  }
+
+  if (key == "clipboard") {
+    const auto text = std::unique_ptr<char, SDL_Deleter>(SDL_GetClipboardText());
+    lua_pushstring(state, text ? text.get() : "");
+    return 1;
+  }
+
+  return lua_pushnil(state), 1;
+}
+
+static int platform_newindex(lua_State *state) noexcept {
+  const std::string_view key = luaL_checkstring(state, 2);
+
+  if (key == "clipboard") {
+    const auto *text = luaL_checkstring(state, 3);
+    SDL_SetClipboardText(text);
+  }
+
+  return 0;
+}
+
+void platform::wire() {
+  lua_newuserdata(L, 1);
+
+  luaL_newmetatable(L, "Platform");
+  lua_pushcfunction(L, platform_index);
+  lua_setfield(L, -2, "__index");
+  lua_pushcfunction(L, platform_newindex);
+  lua_setfield(L, -2, "__newindex");
+
+  lua_setmetatable(L, -2);
+  lua_setglobal(L, "platform");
+}
---
+++ src/web.cpp
@@ -0,0 +1,12 @@
+#include "web.hpp"
+
+static int openurl_call(lua_State *state) noexcept {
+  const auto *url = luaL_checkstring(state, 1);
+  lua_pushboolean(state, SDL_OpenURL(url));
+  return 1;
+}
+
+void web::wire() {
+  lua_pushcfunction(L, openurl_call);
+  lua_setglobal(L, "openurl");
+}
