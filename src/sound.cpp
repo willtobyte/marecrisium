3dad7c0d6fc2551f7479c8b2c7d7c8f647987f6a
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-06 09:33:13 -0300

    Work in progress

--- src/sound.cpp
+++ src/sound.cpp
@@ -27,14 +27,14 @@ namespace {
 
   int sound_on_begin(lua_State* state) {
     auto** ptr = static_cast<sound**>(luaL_checkudata(state, 1, "Sound"));
-    auto* s = *ptr;
+    auto* fx = *ptr;
     luaL_checktype(state, 2, LUA_TFUNCTION);
 
-    if (s->on_begin != LUA_NOREF)
-      luaL_unref(state, LUA_REGISTRYINDEX, s->on_begin);
+    if (fx->on_begin != LUA_NOREF)
+      luaL_unref(state, LUA_REGISTRYINDEX, fx->on_begin);
 
     lua_pushvalue(state, 2);
-    s->on_begin = luaL_ref(state, LUA_REGISTRYINDEX);
+    fx->on_begin = luaL_ref(state, LUA_REGISTRYINDEX);
     return 0;
   }
 
