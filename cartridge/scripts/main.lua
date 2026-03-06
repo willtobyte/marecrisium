6fb0ea7fc808cae2366d2fd56a504f4ec09decd6
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-06 11:28:35 -0300

    Work in progress

--- cartridge/scripts/main.lua
+++ cartridge/scripts/main.lua
@@ -1,8 +1,8 @@
 return {
-	width = 1280,
-	height = 720,
+	width = 1920,
+	height = 1080,
 	title = "My Game",
-	scale = 3.0,
+	scale = 4.0,
 	fullscreen = os.getenv("FULLSCREEN") == "1",
 	on_begin = function()
 		director.navigate("mainmenu")
