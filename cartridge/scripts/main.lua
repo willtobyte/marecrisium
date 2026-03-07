2b0f843f9cd6d31d636e46d55e46514455afa0b7
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-07 13:19:36 -0300

    Work in progress

--- cartridge/scripts/main.lua
+++ cartridge/scripts/main.lua
@@ -3,9 +3,9 @@ return {
 	width = 1920,
 	height = 1080,
 	scale = 4.0,
-	fullscreen = os.getenv("FULLSCREEN") == "1",
 	ticks = 10,
 	gravity = { 0, 980 },
+	fullscreen = os.getenv("FULLSCREEN") == "1",
 	on_begin = function()
 		director.navigate("mainmenu")
 	end,
