1f9692d2a50fe5d2f653a4ebd033a817d84be2ea
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-09 16:56:25 -0300

    Work in progress

--- cartridge/objects/player.lua
+++ cartridge/objects/player.lua
@@ -5,6 +5,7 @@ local jump = -400
 local spin = 360
 local ground = {}
 local grounded = 0
+local riding = {}
 
 return {
 	body = "dynamic",
@@ -32,21 +33,32 @@ return {
 			self.flip = "none"
 		end
 
-		self.vx = vx
-
 		if grounded > 0 and controls.jump then
 			self.vy = jump
+			riding = {}
 		end
+
+		for name, _ in pairs(riding) do
+			local enemy = pool[name]
+			if enemy then
+				vx = vx + enemy.vx
+			end
+		end
+
+		self.vx = vx
 
 		if keyboard.space then
 			self.angle = self.angle + spin * delta
 		end
 	end,
 
-	on_collision_begin = function(self, other_name, _, _, normal_y)
+	on_collision_begin = function(self, other_name, other_kind, _, normal_y)
 		if normal_y and normal_y > 0.5 then
 			ground[other_name] = (ground[other_name] or 0) + 1
 			grounded = grounded + 1
+			if other_kind == "enemy" then
+				riding[other_name] = true
+			end
 		end
 	end,
 
@@ -56,6 +68,7 @@ return {
 			grounded = grounded - 1
 			if ground[other_name] == 0 then
 				ground[other_name] = nil
+				riding[other_name] = nil
 			end
 		end
 	end,
