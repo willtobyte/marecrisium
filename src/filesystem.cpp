4fa35200bf569cfdeac77602676d5737d664be8c
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-11 08:07:14 -0300

    Work in progress

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,14 +1,10 @@
 #include "filesystem.hpp"
 
 void filesystem::mount(std::string_view filename, std::string_view mountpoint) {
-  const auto result = PHYSFS_mount(filename.data(), mountpoint.data(), true);
+  if (!PHYSFS_mount(filename.data(), mountpoint.data(), true)) [[unlikely]] {
+    auto error = std::format(
+      "[PHYSFS_mount] failed to mount {} to {}. reason: {}", filename, mountpoint, PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
 
-  if (!result) [[unlikely]] {
-    const auto* const message = PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode());
-    throw std::runtime_error(
-      std::format("[PHYSFS_mount] failed to mount {} to {}. reason: {}",
-        filename,
-        mountpoint,
-        message));
+    throw std::runtime_error(std::move(error));
   }
 }
