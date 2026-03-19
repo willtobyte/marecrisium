2cb060129a4e307ae0d41739651199df2ee4e74f
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-19 13:25:27 -0300

    Work in progress

--- cartridge/stages/forest.lua
+++ cartridge/stages/forest.lua
@@ -5,8 +5,8 @@ return scheduler.wrap({
 	overlay = "hud",
 	objects = {
 		{ name = "player", kind = "player", x = 100, y = 100 },
-		{ name = "robot", kind = "robot", x = 250, y = 150 },
-		{ name = "drone", kind = "drone", x = 180, y = 200 },
+		-- { name = "robot", kind = "robot", x = 250, y = 150 },
+		-- { name = "drone", kind = "drone", x = 180, y = 200 },
 		{ name = "demon", kind = "demon", x = 300, y = 100 },
 	},
 })
