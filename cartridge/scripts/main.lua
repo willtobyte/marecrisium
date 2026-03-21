2a767edfc3a1b3f1b6edc255c6c8d091999bdb3e
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-21 11:45:14 -0300

    Work in progress

--- cartridge/scripts/main.lua
+++ cartridge/scripts/main.lua
@@ -1,10 +1,12 @@
+local getenv = os.getenv
+
 return {
 	title = "My Game",
 	width = 1920,
 	height = 1080,
 	scale = 3.0,
 	ticks = 10,
-	fullscreen = os.getenv("WINDOWED") ~= "1",
+	fullscreen = getenv("WINDOWED") ~= "1",
 	sentry = "https://b73cc92e6e405d9dc02c2f6f040d6ac7@o4509972952907776.ingest.us.sentry.io/4511022227849216",
 	on_begin = function()
 		-- mouse.shown = false
