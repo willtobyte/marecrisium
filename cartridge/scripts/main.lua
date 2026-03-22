d83aa7d567ac33146c9a1cc3f1e205034376fdf2
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-22 13:56:15 -0300

    Work in progress

--- cartridge/scripts/main.lua
+++ cartridge/scripts/main.lua
@@ -10,6 +10,7 @@ return {
 	sentry = "https://b73cc92e6e405d9dc02c2f6f040d6ac7@o4509972952907776.ingest.us.sentry.io/4511022227849216",
 	on_begin = function()
 		mouse.shown = false
+
 		director.navigate("forest")
 	end,
 }
