417865f9c927a7e1c4ab4a3e5b3ed7b04390e81b
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-22 07:42:37 -0300

    Work in progress

--- cartridge/foregrounds/mist.lua
+++ cartridge/foregrounds/mist.lua
@@ -1,3 +1,13 @@
+local fmod = math.fmod
+
+local LAYERS = {
+	{ speed = 8, alpha = 80, scale = 1.5 },
+	{ speed = 20, alpha = 100, scale = 1.0 },
+	{ speed = 35, alpha = 120, scale = 0.75 },
+}
+
+local offsets = { 0, 0, 0 }
+
 return {
 	pixmaps = { "mist_layer1" },
 
@@ -5,7 +15,12 @@ return {
 		print("foreground: bump!")
 	end,
 
-	on_loop = function(self, delta) end,
+	on_loop = function(self, delta)
+		for i = 1, #LAYERS do
+			local layer = LAYERS[i]
+			offsets[i] = offsets[i] + layer.speed * delta
+		end
+	end,
 
 	on_paint = function(self)
 		local sprite = self.mist_layer1
@@ -14,9 +29,16 @@ return {
 		local vw = viewport.width
 		local vh = viewport.height
 
-		for y = 0, vh - 1, sh do
-			for x = 0, vw - 1, sw do
-				self:draw(sprite, x, y, sw, sh, 200)
+		for i = 1, #LAYERS do
+			local layer = LAYERS[i]
+			local dw = sw * layer.scale
+			local dh = sh * layer.scale
+			local ox = fmod(offsets[i], dw)
+
+			for y = -dh, vh - 1, dh do
+				for x = -dw + ox, vw - 1, dw do
+					self:draw(sprite, x, y, dw, dh, layer.alpha)
+				end
 			end
 		end
 	end,
