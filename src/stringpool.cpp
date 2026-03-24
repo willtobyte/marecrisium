dbaa6658f2c0f287690f6e462612a1c5e602741b
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-24 14:00:14 -0300

    Work in progress

--- src/stringpool.cpp
+++ src/stringpool.cpp
@@ -7,6 +7,5 @@ entt::id_type stringpool::insert(std::string_view value) {
 }
 
 const char* stringpool::get(entt::id_type key) const noexcept {
-  const auto it = _pool.find(key);
-  return it->second.c_str();
+  return _pool.find(key)->second.c_str();
 }
