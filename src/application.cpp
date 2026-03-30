ccf8dfb398ad0dd66d197cf86281c43dc379374f
Rodrigo Delduca <rodrigo@delduca.org>
2026-03-30 12:37:23 -0300

    Work in progress

--- src/application.cpp
+++ src/application.cpp
@@ -4,7 +4,7 @@ int application::run() {
   try {
     const auto* const rom = std::getenv("CARTRIDGE");
     filesystem::mount(rom ? rom : "cartridge.rom", "/");
-    filesystem::try_mount("mods", "/");
+    filesystem::try_mount("modifications", "/");
 
     auto se = scriptengine();
     se.run();
