937754e55a9a4270218ce16e980f3ea30cb2bd53
Rodrigo Delduca <rodrigo@delduca.org>
2026-04-20 07:14:40 -0300

    Work in progress

--- src/application.cpp
+++ src/application.cpp
@@ -1,13 +1,32 @@
 #include "application.hpp"
 
 int application::run() {
-  const auto* const rom = std::getenv("CARTRIDGE");
-  filesystem::mount(rom ? rom : "cartridge.rom", "/");
-  filesystem::try_mount("modifications", "/");
-  filesystem::try_mount("modifications.zip", "/");
+  try {
+    const auto* const rom = std::getenv("CARTRIDGE");
+    filesystem::mount(rom ? rom : "cartridge.rom", "/");
+    filesystem::try_mount("modifications", "/");
+    filesystem::try_mount("modifications.zip", "/");
 
-  scriptengine se;
-  se.run();
+    scriptengine se;
+    se.run();
+  } catch (const std::exception& exc) {
+    const auto* const error = exc.what();
+
+    std::println(stderr, "{}", error);
+
+#ifndef DEBUG
+    const auto event = sentry_value_new_event();
+    const auto exception = sentry_value_new_exception("exception", error);
+    sentry_value_set_stacktrace(exception, nullptr, 0);
+    sentry_event_add_exception(event, exception);
+    sentry_capture_event(event);
+    sentry_flush(3000);
+#endif
+
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Ink Spill Disaster", error, nullptr);
+
+    return 1;
+  }
 
   return 0;
 }
