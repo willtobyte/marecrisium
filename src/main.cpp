4e7dab287e2e13cba87cb1005eebc44ccc64554e
Rodrigo Delduca <rodrigo@delduca.org>
2026-04-22 10:26:59 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,25 @@ int main(int argc, char** argv) {
 #endif
 
   SDL_Init(SDL_INIT_GAMEPAD | SDL_INIT_VIDEO);
-  std::atexit(+[] { SDL_Quit(); });
+  std::atexit([] { SDL_Quit(); });
 
   PHYSFS_init(argv[0]);
   PHYSFS_registerArchiver(&archiver);
-  std::atexit(+[] { PHYSFS_deinit(); });
+  std::atexit([] { PHYSFS_deinit(); });
 
   auto config = ma_engine_config_init();
   config.channels = 2;
   config.sampleRate = 48000;
   config.periodSizeInFrames = 2048;
   ma_engine_init(&config, &audio);
-  std::atexit(+[] { ma_engine_uninit(&audio); });
+  std::atexit([] { ma_engine_uninit(&audio); });
 
   L = luaL_newstate();
   luaL_openlibs(L);
-  std::atexit(+[] { lua_close(L); });
+  std::atexit([] { lua_close(L); });
 
   SteamAPI_InitSafe();
-  std::atexit(+[] { SteamAPI_Shutdown(); });
+  std::atexit([] { SteamAPI_Shutdown(); });
 
   resources storage;
   depot = &storage;
