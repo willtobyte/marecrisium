1449444c4afb4b4d90a27f93c82f0161c042a2cb
Rodrigo Delduca <rodrigo@delduca.org>
2026-05-04 13:37:42 -0300

    Work in progress

--- cartridge/scripts/helpers/spawner.lua
+++ cartridge/scripts/helpers/spawner.lua
@@ -1,8 +1,7 @@
 local spawner = {}
 
 function spawner.at_center(name, kind, center_x, center_y)
-	world.spawn(name, kind, center_x, center_y)
-	local instance = pool[name]
+	local instance = world.spawn(name, kind, center_x, center_y)
 	if instance then
 		instance.x = instance.x + (center_x - instance.center_x)
 		instance.y = instance.y + (center_y - instance.center_y)
