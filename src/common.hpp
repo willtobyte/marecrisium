f23a81df1474caf46286a33d842fec430398f574
Rodrigo Delduca <rodrigo@delduca.org>
2026-05-29 18:14:30 -0300

    Work in progress

--- src/common.hpp
+++ src/common.hpp
@@ -72,14 +72,3 @@ struct viewport {
 };
 
 extern struct viewport viewport;
-
-template <typename T>
-[[nodiscard("Angle conversion has no side effects")]]
-constexpr T to_radians(T degrees) {
-  if consteval {
-    return degrees * (std::numbers::pi_v<T> / T{180});
-  } else {
-    static constinit auto factor = std::numbers::pi_v<T> / T{180};
-    return degrees * factor;
-  }
-}
--- src/math.hpp
+++ src/math.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
 void sincos(float x, float& sine, float& cosine);
+
+template <typename T>
+[[nodiscard("Angle conversion has no side effects")]]
+constexpr T to_radians(T degrees) {
+  if consteval {
+    return degrees * (std::numbers::pi_v<T> / T{180});
+  } else {
+    static constinit auto factor = std::numbers::pi_v<T> / T{180};
+    return degrees * factor;
+  }
+}
