149b6f61951930689bbc518ac61627ecb48f7f2f
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-20 16:03:44 -0300

    Work in progress

--- cartridge/scripts/helpers/controls.lua
+++ cartridge/scripts/helpers/controls.lua
@@ -1,15 +1,16 @@
 local controls = {}
+local deadzone = 0.5
 
 setmetatable(controls, {
 	__index = function(_, key)
 		if key == "left" then
-			return gamepad.left_x < -0.5 or gamepad.left or keyboard.a or keyboard.left
+			return gamepad.left_x < -deadzone or gamepad.left or keyboard.a or keyboard.left
 		elseif key == "right" then
-			return gamepad.left_x > 0.5 or gamepad.right or keyboard.d or keyboard.right
+			return gamepad.left_x > deadzone or gamepad.right or keyboard.d or keyboard.right
 		elseif key == "up" then
-			return gamepad.left_y < -0.5 or gamepad.up or keyboard.w or keyboard.up
+			return gamepad.left_y < -deadzone or gamepad.up or keyboard.w or keyboard.up
 		elseif key == "down" then
-			return gamepad.left_y > 0.5 or gamepad.down or keyboard.s or keyboard.down
+			return gamepad.left_y > deadzone or gamepad.down or keyboard.s or keyboard.down
 		elseif key == "action" then
 			return gamepad.south or keyboard.space
 		elseif key == "dismiss" then
