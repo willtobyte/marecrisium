831123a1482fd1e0b5c6cbdcc2e58a47b851a267
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-20 14:02:18 -0300

    Work in progress

---
+++ cartridge/scripts/helpers/controls.lua
@@ -0,0 +1,21 @@
+local threshold = 0.5
+
+local controls = {}
+
+setmetatable(controls, {
+	__index = function(_, key)
+		if key == "left" then
+			return gamepad.left_x < -threshold or gamepad.left or keyboard.a
+		elseif key == "right" then
+			return gamepad.left_x > threshold or gamepad.right or keyboard.d
+		elseif key == "up" then
+			return gamepad.left_y < -threshold or gamepad.up or keyboard.w
+		elseif key == "down" then
+			return gamepad.left_y > threshold or gamepad.down or keyboard.s
+		elseif key == "space" then
+			return gamepad.south or keyboard.space
+		end
+	end,
+})
+
+return controls
