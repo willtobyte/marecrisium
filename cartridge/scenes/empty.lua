cfa1bdedf5f4777a00964dff78a872700ba0d302
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-20 16:59:07 -0300

    Work in progress

--- cartridge/scenes/empty.lua
+++ cartridge/scenes/empty.lua
@@ -1,1 +1,31 @@
-return {}
+local navigation = require("helpers/navigation")
+local navigator
+
+return {
+	-- objects = {
+	-- 	{ name = "walkie_center", kind = "walkietalkie", x = 224, y = 119 },
+	-- 	{ name = "walkie_north", kind = "walkietalkie", x = 224, y = 39 },
+	-- 	{ name = "walkie_east", kind = "walkietalkie", x = 384, y = 119 },
+	-- 	{ name = "walkie_south", kind = "walkietalkie", x = 224, y = 199 },
+	-- 	{ name = "walkie_west", kind = "walkietalkie", x = 64, y = 119 },
+	-- },
+
+	-- on_enter = function()
+	-- 	navigator = navigation.new({
+	-- 		pool.walkie_center,
+	-- 		pool.walkie_north,
+	-- 		pool.walkie_east,
+	-- 		pool.walkie_south,
+	-- 		pool.walkie_west,
+	-- 	})
+	-- end,
+
+	-- on_leave = function()
+	-- 	navigator:clear()
+	-- 	navigator = nil
+	-- end,
+
+	-- on_loop = function()
+	-- 	navigator:update()
+	-- end,
+}
---
+++ cartridge/scripts/helpers/navigation.lua
@@ -0,0 +1,90 @@
+local controls = require("helpers/controls")
+local navigator = {}
+local navigation = {}
+
+navigator.__index = navigator
+
+function navigation.new(objects)
+	return setmetatable({
+		objects = objects,
+		left = controls.left,
+		right = controls.right,
+		up = controls.up,
+		down = controls.down,
+	}, navigator)
+end
+
+function navigator:update()
+	local left = controls.left
+	local right = controls.right
+	local up = controls.up
+	local down = controls.down
+	local dx = 0
+	local dy = 0
+
+	if left and not self.left then
+		dx = -1
+	elseif right and not self.right then
+		dx = 1
+	elseif up and not self.up then
+		dy = -1
+	elseif down and not self.down then
+		dy = 1
+	end
+
+	self.left = left
+	self.right = right
+	self.up = up
+	self.down = down
+
+	if dx == 0 and dy == 0 then
+		return
+	end
+
+	local objects = self.objects
+	local current = self.current
+	if not current then
+		current = objects[1]
+		self.current = current
+		current:select()
+		return
+	end
+
+	local x = current.x
+	local y = current.y
+	local winner
+	local nearest
+
+	for i = 1, #objects do
+		local object = objects[i]
+		local ox = object.x - x
+		local oy = object.y - y
+
+		if ox * dx + oy * dy > 0 then
+			local distance = ox * ox + oy * oy
+			if not nearest or distance < nearest then
+				winner = object
+				nearest = distance
+			end
+		end
+	end
+
+	if not winner then
+		return
+	end
+
+	current:unselect()
+	self.current = winner
+	winner:select()
+end
+
+function navigator:clear()
+	if self.current then
+		self.current:unselect()
+	end
+
+	self.objects = nil
+	self.current = nil
+end
+
+return navigation
