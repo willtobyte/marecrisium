4ec5f6bfe09591699798e60280c274017b0759d0
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-21 16:40:21 -0300

    Work in progress

--- src/common.cpp
+++ src/common.cpp
@@ -1,7 +1,13 @@
 lua_State *L = nullptr;
+
 SDL_Renderer *renderer = nullptr;
+
 ma_engine audio{};
+
 struct viewport viewport{};
+
 struct depot *depot = nullptr;
+
 const int slot{-3};
+
 const std::uint64_t boot{SDL_GetPerformanceCounter()};
