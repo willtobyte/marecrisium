2774b82118c2a877338489901179f6c3cf0497b8
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-21 10:33:55 -0300

    Work in progress

--- assets/tools/convert.py
+++ assets/tools/convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S uv run --script
 # /// script
-# requires-python = ">=3.13"
+# requires-python = ">=3.14"
 # dependencies = ["jinja2", "pyoxipng"]
 # ///
 """Convert TexturePacker JSON output into a Lua object."""
