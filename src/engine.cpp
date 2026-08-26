5c5d2b95951e6ac0bab1743d76e247888992ef0f
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-26 09:58:06 -0300

    Work in progress

--- src/engine.cpp
+++ src/engine.cpp
@@ -34,9 +34,11 @@ engine::engine() {
 
   lua_pop(L, 1);
 
+  const auto vsync = std::getenv("NOVSYNC") == nullptr;
+
   const auto properties = SDL_CreateProperties();
   SDL_SetPointerProperty(properties, SDL_PROP_RENDERER_CREATE_WINDOW_POINTER, window);
-  SDL_SetNumberProperty(properties, SDL_PROP_RENDERER_CREATE_PRESENT_VSYNC_NUMBER, true);
+  SDL_SetNumberProperty(properties, SDL_PROP_RENDERER_CREATE_PRESENT_VSYNC_NUMBER, vsync);
   SDL_SetStringProperty(properties, SDL_PROP_RENDERER_CREATE_NAME_STRING, nullptr);
 
   renderer = SDL_CreateRendererWithProperties(properties);
