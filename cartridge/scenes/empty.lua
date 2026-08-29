acb1aec161e2f49d649a25f2b6969ab018c1fce4
Rodrigo Delduca <rodrigo@delduca.org>
2026-08-29 17:03:05 -0300

    Work in progress

--- cartridge/scenes/empty.lua
+++ cartridge/scenes/empty.lua
@@ -16,7 +16,7 @@ return {
 			pool.walkie_southwest,
 			pool.walkie_southeast,
 		})
-		timer:singleshot(60000, function()
+		timer:singleshot(1000, function()
 			error("Intentional error after 60 seconds")
 		end)
 	end,
