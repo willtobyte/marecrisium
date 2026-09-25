c75f91001300cbf57eeadac03ca6cabfe458d026
Rodrigo Delduca <rodrigo@delduca.org>
2026-09-25 15:18:08 -0300

    Work in progress

--- cartridge/scenes/gatehouse.lua
+++ cartridge/scenes/gatehouse.lua
@@ -9,13 +9,13 @@ return {
 		{ name = "walkietalkie", kind = "walkietalkie", x = 0, y = 0 },
 		{ name = "datapad", kind = "datapad", x = 0, y = 0 },
 		{ name = "idmanual", kind = "idmanual", x = 0, y = 0 },
-		{ name = "button", kind = "button", x = 0, y = 0 },
+		{ name = "panicbutton", kind = "panicbutton", x = 0, y = 0 },
 		{ name = "alarmlight", kind = "alarmlight", x = 0, y = 0 },
 	},
 
 	on_enter = function()
-		navigator = navigation.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.button })
-		interact = interaction.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.button })
+		navigator = navigation.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.panicbutton })
+		interact = interaction.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.panicbutton })
 
 		pool.walkietalkie.on_hover = function(object)
 			print("ON HOVER")
@@ -43,11 +43,11 @@ return {
 			navigation.unselect(object)
 		end
 
-		pool.button.on_hover = function(object)
+		pool.panicbutton.on_hover = function(object)
 			navigation.select(object)
 		end
 
-		pool.button.on_unhover = function(object)
+		pool.panicbutton.on_unhover = function(object)
 			navigation.unselect(object)
 		end
 	end,
