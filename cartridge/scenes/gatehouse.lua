fc6bfc1abf333942c24f685dc59d68dff8b26d25
Rodrigo Delduca <rodrigo@delduca.org>
2026-09-25 15:05:55 -0300

    Work in progress

--- cartridge/scenes/gatehouse.lua
+++ cartridge/scenes/gatehouse.lua
@@ -7,15 +7,15 @@ return {
 	objects = {
 		{ name = "paperpiece", kind = "paperpiece", x = 0, y = 0 },
 		{ name = "walkietalkie", kind = "walkietalkie", x = 0, y = 0 },
-		{ name = "pda", kind = "pda", x = 0, y = 0 },
+		{ name = "datapad", kind = "datapad", x = 0, y = 0 },
 		{ name = "idmanual", kind = "idmanual", x = 0, y = 0 },
 		{ name = "button", kind = "button", x = 0, y = 0 },
 		{ name = "alarmlight", kind = "alarmlight", x = 0, y = 0 },
 	},
 
 	on_enter = function()
-		navigator = navigation.new({ pool.walkietalkie, pool.pda, pool.idmanual, pool.button })
-		interact = interaction.new({ pool.walkietalkie, pool.pda, pool.idmanual, pool.button })
+		navigator = navigation.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.button })
+		interact = interaction.new({ pool.walkietalkie, pool.datapad, pool.idmanual, pool.button })
 
 		pool.walkietalkie.on_hover = function(object)
 			print("ON HOVER")
@@ -27,11 +27,11 @@ return {
 			navigation.unselect(object)
 		end
 
-		pool.pda.on_hover = function(object)
+		pool.datapad.on_hover = function(object)
 			navigation.select(object)
 		end
 
-		pool.pda.on_unhover = function(object)
+		pool.datapad.on_unhover = function(object)
 			navigation.unselect(object)
 		end
 
